ece721e2aaf9a63504e842b07d61b035790985df
jojo3420 <jjjhhhvvv@gmail.com>
2020-01-18 16:14:22 +0900

    eslint 적용 커밋 누락분 커밋

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,17 @@
 
 const newArr = [1, 2, 3, 4, 5].map(n => n * 2);
 console.log('newArr: ', newArr);
+
+const books = [
+  { title: 'Learning Javascript', publisher: 'AAA', page: 200 },
+  { title: 'Learning React', publisher: 'BBB', page: 300 },
+  { title: 'Learning Nodejs', publisher: 'CCC', page: 444 },
+];
+
+function say ({ title, publisher, page }) {
+  console.log(`title: ${title}, publisher: ${publisher}, page: ${page}`);
+}
+
+for (const book of books) {
+  say(book);
+}
